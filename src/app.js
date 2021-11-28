7ec6102642b1d90b06dc19098fa6f7186dfd588d
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2021-11-28 15:51:45 +0100

    added GuessNumber game module

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import "./styles.css";
 import { ContextMenu } from "./menu";
 import { BackgroundModule } from "./modules/background.module";
-// import { ClicksModule } from "./modules/clicks.module";
-// import { ShapeModule } from "./modules/shape.module";
 import { GameOfLife } from "./modules/gol";
 import { RandomSound } from "./modules/randomSound.module";
+import { GuessNumber } from "./modules/guessNumber.module";
 
 // Инициализируем ContextMenu
 const contextMenu = new ContextMenu("#menu");
@@ -24,12 +23,10 @@ const bgModule = new BackgroundModule(
   "Generate random background"
 );
 const randomSound = new RandomSound("Sounds", "Random Sounds");
-// const clicksModule = new ClicksModule("Clicks", "Generate clicks");
-// const shapeModule = new ShapeModule("Shapes", "Generate shapes");
+const guessNumber = new GuessNumber("numbers", "Отгадай число");
 
 // добавляем модуль в контекстное меню
 contextMenu.add(gameOfLife);
 contextMenu.add(bgModule);
-// contextMenu.add(clicksModule);
-// contextMenu.add(shapeModule);
 contextMenu.add(randomSound);
+contextMenu.add(guessNumber);
