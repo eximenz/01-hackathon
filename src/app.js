905e0a033d2afbfeafa93e769155a6d4dff581aa
eximenz <terpigoreva1992@gmail.com>
2021-11-28 23:15:10 +0300

    added gameOfLife to the begining

--- src/app.js
+++ src/app.js
@@ -7,10 +7,8 @@ import { GuessNumber } from "./modules/guessNumber.module";
 import { MemoryGame } from "./modules/memoryGame.module";
 import { ImgModule } from "./modules/img.module";
 
-// Инициализируем ContextMenu
 const contextMenu = new ContextMenu("#menu");
 
-// добавляем слушатели для вызова контекстного меню и его закрытия
 document.body.addEventListener("contextmenu", (event) => {
   contextMenu.open(event);
 });
@@ -19,8 +17,7 @@ document.body.addEventListener("click", (event) => {
   contextMenu.close(event);
 });
 
-// создаем инстансы модулей
-const gameOfLife = new GameOfLife("Game of Life", "Жизнь бактерий");
+const gameOfLife = new GameOfLife("Game of Life", "Окрестности Мура");
 const bgModule = new BackgroundModule("Background", "Случайная смена фона");
 const randomSound = new RandomSound("Sounds", "Случайные звуки");
 const guessNumber = new GuessNumber("numbers", "Отгадай число");
@@ -28,10 +25,9 @@ const memoryGame = new MemoryGame("Memory", "Игра на память");
 const imgModule = new ImgModule("imgModule", "Получи дозу радости");
 
 
-// добавляем модуль в контекстное меню
-contextMenu.add(gameOfLife);
 contextMenu.add(bgModule);
 contextMenu.add(randomSound);
 contextMenu.add(guessNumber);
 contextMenu.add(memoryGame);
 contextMenu.add(imgModule);
+contextMenu.add(gameOfLife);
