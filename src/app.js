dc775a9e28d46d21a88cb7e03b745eaf7d9d7eaf
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2021-11-28 19:20:18 +0100

    new memory game added

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { BackgroundModule } from "./modules/background.module";
 import { GameOfLife } from "./modules/gol";
 import { RandomSound } from "./modules/randomSound.module";
 import { GuessNumber } from "./modules/guessNumber.module";
+import { MemoryGame } from "./modules/memoryGame.module";
 
 // Инициализируем ContextMenu
 const contextMenu = new ContextMenu("#menu");
@@ -18,16 +19,18 @@ document.body.addEventListener("click", (event) => {
 });
 
 // создаем инстансы модулей
-const gameOfLife = new GameOfLife("Game of Life", "Let's watch and relax!");
+const gameOfLife = new GameOfLife("Game of Life", "Жизнь бактерий");
 const bgModule = new BackgroundModule(
   "Background",
   "Generate random background"
 );
-const randomSound = new RandomSound("Sounds", "Random Sounds");
+const randomSound = new RandomSound("Sounds", "Случайные звуки");
 const guessNumber = new GuessNumber("numbers", "Отгадай число");
+const memoryGame = new MemoryGame("Memory", "Игра на память");
 
 // добавляем модуль в контекстное меню
 contextMenu.add(gameOfLife);
 contextMenu.add(bgModule);
 contextMenu.add(randomSound);
 contextMenu.add(guessNumber);
+contextMenu.add(memoryGame);
